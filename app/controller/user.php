05cacc02adf294aadbaffbdbdc1f789fe5931bd8
Alan Hardman <alanaktion@gmail.com>
2015-10-30 19:31:27 -0600

    Adding base user action

--- app/controller/user.php
+++ app/controller/user.php
@@ -4,4 +4,15 @@ namespace Controller;
 
 class User extends \Controller {
 
+	/**
+	 * GET /u/@username
+	 * @param Base $f3
+	 * @param array $params
+	 */
+	function base($f3, $params) {
+		$user = \Helper\Api\User::get($params['username']);
+		$f3->set('this_user', $user);
+		$this->_render('user/single.html');
+	}
+
 }
